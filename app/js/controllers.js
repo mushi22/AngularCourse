b6f50b512cbf78a12bff053d71aac0f34aae1fbd
mushi22 <mushi22@github.com>
2014-12-30 16:43:02 +1100

    completed

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,56 +6,47 @@ angular.module('myApp.controllers', [])
 	.controller('LandingPageController', [function() {
 
 	}])
-	.controller('WaitlistController', ['$scope', '$firebase', 'FIREBASE_URL',  function($scope, $firebase, FIREBASE_URL) {
-		//connecting scope.parties to live firebase data
-		var partiesRef = new Firebase(FIREBASE_URL + 'parties');
-		$scope.parties = $firebase(partiesRef);
+	.controller('WaitlistController', ['$scope', 'partyService', 'textMessageService', 'authService',  function($scope, partyService, textMessageService, authService) {
+
+		//bind users parties to $scope.parties
+		authService.getCurrentUser().then(function(user) {
+			if(user) {
+				$scope.parties = partyService.getPartiesByUserId(user.id);
+			}
+		})
+
 		//Object to store data from waitlist form
 		$scope.newParty = {name: '', phone: '', size: '', done: false, notified: 'No'};
 
 		//function to save new party to wait list
 		$scope.saveParty = function(){
-			$scope.parties.$add($scope.newParty);
-			$scope.newParty = {name: '', phone: '', size: '', done:false, notified:'No'};
+			partyService.saveParty($scope.newParty, $scope.currentUser.id);
+			$scope.newParty = {name: '', phone: '', size: '', done: false, notified: 'No'};
 		};
 
 		//Function to send text message to a party. 
 		$scope.sendTextMessage = function(party) {
-			var textMessageRef = new Firebase(FIREBASE_URL + 'textMessages');
-			var textMessages = $firebase(textMessageRef);
-			var newTextMessage = {
-				phoneNumber: party.phone,
-				size: party.size,
-				name: party.name
-			};
-			textMessages.$add(newTextMessage);
-			//code here for notified
-			party.notified = 'Yes';
-			$scope.parties.$save(party.$id);
+			textMessageService.sendTextMessage(party, $scope.currentUser.id);
 		};
 	}])
 
-	.controller('AuthController', ['$scope', '$firebaseSimpleLogin','$location','FIREBASE_URL','authService', function($scope, $firebaseSimpleLogin, $location, FIREBASE_URL, authService) {
-		var authRef = new Firebase(FIREBASE_URL);
+	.controller('AuthController', ['$scope','authService', function($scope, authService) {
 
-		var auth = $firebaseSimpleLogin(authRef);
-
+		//object bound to inputs on the register and login pages
 		$scope.user = {email: '', password: ''};
 
+		//meth to register a new user using authService
 		$scope.register = function () {
-			auth.$createUser($scope.user.email, $scope.user.password).then(function(data) {
-				console.log(data);
-				$scope.login();
-			});
+			authService.register($scope.user);
 		};
 
+		//method to login user
 		$scope.login = function () {
 			authService.login($scope.user);
 		};
 
+		//method to logout 
 		$scope.logout = function() {
-			auth.$logout();
-			//redirect users to landing page
-			$location.path('/');
+			authService.logout();
 		};
 	}]);
--- app/js/services.js
+++ app/js/services.js
@@ -7,17 +7,90 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
 	.value('FIREBASE_URL', 'https://waitandeat-sohaib.firebaseio.com/')
-	.factory('authService', function($firebaseSimpleLogin, $location, FIREBASE_URL) {
+	.factory('dataService', function($firebase, FIREBASE_URL) {
+		var dataRef = new Firebase(FIREBASE_URL);
+		var fireData = $firebase(dataRef);
+
+		return fireData;
+	})
+	.factory('partyService', function(dataService) {
+		var users = dataService.$child('users');
+
+		var partyServiceObject = {
+			saveParty: function(party, userId) {
+				//parties.$add(party);
+				users.$child(userId).$child('parties').$add(party);
+			},
+			getPartiesByUserId: function(userId){
+				return users.$child(userId).$child('parties');
+			}
+		};
+
+		return partyServiceObject;
+	})
+	.factory('textMessageService', function(dataService, partyService) {
+
+		var textMessages = dataService.$child('textMessages');
+
+		var textMessageServiceObject = {
+			sendTextMessage: function(party, userId) {
+				var newTextMessage = {
+					phoneNumber: party.phone,
+					size: party.size,
+					name: party.name
+				};
+				textMessages.$add(newTextMessage);
+				//party.notified = 'Yes';
+				//partyService.parties.$save(party.$id);
+				partyService.getPartiesByUserId(userId).$child(party.$id).$update({notified: 'Yes'});
+
+			}
+		};
+
+		return textMessageServiceObject;
+
+	})
+	.factory('authService', function($firebaseSimpleLogin, $location,$rootScope, FIREBASE_URL, dataService) {
 		var authRef = new Firebase(FIREBASE_URL);
 		var auth = $firebaseSimpleLogin(authRef);
-		
-		return {
+		var emails = dataService.$child('emails');
+
+		var authServiceObject = {
+			register:function(user) {
+				auth.$createUser(user.email, user.password).then(function(data) {
+					console.log(data);
+					emails.$add({email: user.email});
+					authServiceObject.login(user);
+				});
+
+			},
 			login: function(user) {
 				auth.$login('password', user).then(function(data){
 				console.log(data);
 				//redirect to waitlist page
 				$location.path('/waitlist');
 			});
+			},
+			logout: function() {
+				auth.$logout();
+				$location.path('/');
+			},
+
+			getCurrentUser: function() {
+				return auth.$getCurrentUser();
 			}
+
 		};
+
+		$rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
+			//console.log("User"+ user.id + " successfully logged in!") //save currentUser on our rootscope
+			$rootScope.currentUser = user;
+		});
+
+		$rootScope.$on("$firebaseSimpleLogin:logout", function() {
+			//console.log("User"+ user.id + " successfully logged in!") //save currentUser on our rootscope
+			$rootScope.currentUser = null;
+		});
+
+		return authServiceObject;
 	});
